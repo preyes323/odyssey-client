0611beb2d13f617faf09c653e01194e04daf68ed
Victor Paolo Reyes <vpaoloreyes@gmail.com>
2023-02-09 16:47:33 +0800

    complete URI

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from "./pages";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "catstronaut-server-production-ab48.up.railway.app",
+  uri: "https://catstronaut-server-production-ab48.up.railway.app",
   cache: new InMemoryCache(),
 });
 
